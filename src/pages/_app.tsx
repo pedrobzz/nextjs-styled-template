a0279c181b6bc37e5889c8fee1c3058f0cd9179d
pedrobzz <pedrobzz66@gmail.com>
2022-04-08 23:48:29 -0300

    Revert "first commit"
    This reverts commit a91cc5dea95f5f42cf477cb7329a932513cf53a3.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,11 @@
 import type { AppProps } from "next/app";
 
 import { AppContextProvider } from "../common/context/appContext";
-import GlobalStyle from "../common/styles/styles";
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
     <>
       <AppContextProvider>
-        <GlobalStyle />
         <Component {...pageProps} />
       </AppContextProvider>
     </>
